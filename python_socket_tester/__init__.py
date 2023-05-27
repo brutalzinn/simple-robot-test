68c4e410722cac4a4f883d383736acd157923dc5
Roberto Paes <robertocpaes@gmail.com>
2023-05-27 12:20:37 -0300

    add web socket server to easily tests

---
+++ python_socket_tester/__init__.py
@@ -0,0 +1,42 @@
+from simple_websocket_server import WebSocketServer, WebSocket
+import time, traceback
+import threading
+
+#https://gist.github.com/allanfreitas/e2cd0ff49bbf7ddf1d85a3962d577dbf
+def every(delay, task):
+  next_time = time.time() + delay
+  while True:
+    time.sleep(max(0, next_time - time.time()))
+    try:
+      task()
+    except Exception:
+      traceback.print_exc()
+    next_time += (time.time() - next_time) // delay * delay + delay
+
+class SimpleEcho(WebSocket):
+    def __init__(self, server, sock, address):
+       self.batteryIndicator = 7200
+       super().__init__(server, sock, address)
+    def handle(self):
+        # echo message back to client
+        self.send_message(self.data)
+    def batteryLevel(self):
+       #7v is the max
+       #4v is already charged but low level chargeded
+       voltage = self.batteryIndicator / 1000
+       self.batteryIndicator = self.batteryIndicator - 100
+       print(voltage)
+       if(self.batteryIndicator== 1000):
+          self.batteryIndicator= 7200
+       self.send_message(str({"battery_level": voltage}))
+
+    def connected(self):
+        threading.Thread(target=lambda: every(0.5, self.batteryLevel)).start()
+        print(self.address, 'connected')
+
+    def handle_close(self):
+        print(self.address, 'closed')
+
+
+server = WebSocketServer('', 6666, SimpleEcho)
+server.serve_forever()
